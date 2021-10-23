28c7410c3c5fd10da3da5bf4b7d2769152d9d938
annmro <92086129+annmro@users.noreply.github.com>
2021-10-23 20:41:45 +0200

    add cors

--- api/app.ts
+++ api/app.ts
@@ -4,11 +4,13 @@ import { CardController } from './controllers/CardController';
 import { UserController } from './controllers/UserController';
 import express, { Application } from 'express';
 import { toNumber } from 'lodash';
+import cors from 'cors';
 
 const app: Application = express();
 const portStr: string = process.env.PORT || '3000';
 const port: number = toNumber(portStr);
 
+app.use(cors());
 app.listen(port, () => {
     console.log(`Server listening on the port::${port}`);
 });
