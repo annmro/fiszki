860e4ee63b56ef9aaf7d24e81b919ffd8406b15a
annmro <92086129+annmro@users.noreply.github.com>
2021-10-23 12:50:52 +0200

    change port

--- api/app.ts
+++ api/app.ts
@@ -3,9 +3,11 @@ import { DeckController } from './controllers/DeckController'
 import { CardController } from './controllers/CardController';
 import { UserController } from './controllers/UserController';
 import express, { Application } from 'express';
+import { toNumber } from 'lodash';
 
 const app: Application = express();
-const port: number = 3000;
+const portStr: string = process.env.PORT || '3000';
+const port: number = toNumber(portStr);
 
 app.listen(port, () => {
     console.log(`Server listening on the port::${port}`);
